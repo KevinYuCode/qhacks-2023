5aebd23a4402f76cba98df9d4aca5a073153b9d4
SamEThibault <sam5thibault@gmail.com>
2023-01-28 12:31:31 -0500

    session and suggestions endpoint added

--- backend/server.py
+++ backend/server.py
@@ -1,21 +1,45 @@
-from flask import Flask, request
+import os
+from flask import Flask, request, session
 from flask_cors import CORS, cross_origin
+from dotenv import load_dotenv
 
+
+load_dotenv()
 app = Flask(__name__)
 cors = CORS(app)
 
 app.config['CORS_HEADERS'] = 'Content-Type'
+app.config['SECRET_KEY'] =  os.getenv("secret_key")
 
+# To call OpenAI API and Google API (main endpoint)
+# Requires: json data: {"prompt" : "A chatGPT prompt"}
 @app.route("/response", methods=["POST"])
 @cross_origin()
 def get_response():
-    client_data = request.json
-    print(client_data['data'])
+    # At this point, use prompt to call OpenAI API and Google Suggestions API (parallel if possible)
+    prompt = request.json['prompt']
+    res = prompt
+
+    # First clear past suggestions, then get new ones
+    session.pop('suggestions', default=None)
+    session['suggestions']  = ["testing1", "testing2", "testing3"]
     return {
-        "What you have sent" : client_data['data']
+        "response" : res,
+        "suggestions": session['suggestions']
     }
 
-# @app.route()
+
+# To call OpenAI API for all recent suggestions
+@app.route("/suggestions", methods=["GET"])
+@cross_origin()
+def get_suggestions():
+    # Call API (in parallel ideally) for each suggestion
+    suggestions_res = []
+    for suggestion in session['suggestions']:
+        suggestions_res.append("reponse")
+    
+    return {"suggestion_responses": suggestions_res}
+    
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
