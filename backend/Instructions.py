1ee7e2413baee8dc98a9b613e3417b534aa75d4f
LoganHoogendijk <logan.hoogendijk@gmail.com>
2023-01-28 16:38:34 -0500

    Can get images

--- backend/Instructions.py
+++ backend/Instructions.py
@@ -10,11 +10,24 @@ def prompt_response(prompt):
     COMPLETIONS_MODEL = "text-davinci-003"
 
     response = openai.Completion.create(
-    prompt=prompt,
-    temperature=0,
-    max_tokens=300,
-    model=COMPLETIONS_MODEL,
-    stop=["7."]
+        prompt=prompt,
+        temperature=0,
+        max_tokens=300,
+        model=COMPLETIONS_MODEL,
+        stop=["7."]
     )["choices"][0]["text"].strip(" \n")
 
     return response.split("\n\n")
+
+
+def prompt_image(prompt):
+    load_dotenv()
+    openai.api_key = os.getenv("open-ai-key")
+
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="512x512"
+    )
+    image_url = response['data'][0]['url']
+    return image_url
